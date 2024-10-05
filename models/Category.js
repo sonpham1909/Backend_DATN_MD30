bee718e2da6ed6d1d50bf040a4b3b7e9e6ec5902
conghoang204 <146950585+conghoang204@users.noreply.github.com>
2024-10-05 20:59:52 +0700

    avatar category

--- models/Category.js
+++ models/Category.js
@@ -10,7 +10,9 @@ const CategorySchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-
+    imgcategory: {
+        type: String, // Đường dẫn ảnh
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Category', CategorySchema);

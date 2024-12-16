727fa8b1afdac7fa739528259fe15646454492ba
sonpham1909 <145566152+sonpham1909@users.noreply.github.com>
2024-12-16 22:03:47 +0700

    oke

--- controller/payment_momoController.js
+++ controller/payment_momoController.js
@@ -200,8 +200,8 @@ const config = {
   secretKey: "K951B6PE1waDMi640xX08PD3vg6EkVlz",
   partnerCode: "MOMO",
   redirectUrl: "yourapp://payment-success",
-1
-  ipnUrl: "https://292e-2401-d800-5140-685d-2173-10ab-fe82-5dba.ngrok-free.app/v1/Payment_Momo/callback",
+
+  ipnUrl: "https://6b45-2401-d800-7330-1a24-7da3-c5b7-aacf-b011.ngrok-free.app/v1/Payment_Momo/callback",
 
   requestType: "payWithATM",
   lang: "vi",
